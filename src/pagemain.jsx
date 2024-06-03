aece2be4353b0f9b063016bd28790acc55787b47
Aram <aram.anto@gmail.com>
2024-06-03 14:36:43 +0200

    first commit

---
+++ src/pagemain.jsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import App from "./App";
+import Blog from "./Blog";
+import theme from "./theme";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Blog />
+    </ThemeProvider>
+    ,
+  </React.StrictMode>
+);
